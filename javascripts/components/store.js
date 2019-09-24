1790804bb86a81032a6ae577c59c75f18e20d330
Randy Tate <randytatejr@gmail.com>
2019-09-23 19:55:43 -0500

    adding info

--- javascripts/components/store.js
+++ javascripts/components/store.js
@@ -1,11 +1,27 @@
+const bookInfo = {
+    price: 24.99,
+    title: "Fifty Shades of Chicken",
+    image: "./assets/images/book.jpg"
+  };
+
 const printToDom = (divId, textToPrint) => {
     const divContainer = document.getElementById(divId);
     divContainer.innerHTML = textToPrint;
 };
 
+const addToCartEvent = () => {
+    console.log ('Added to cart');
+};
+
 const makeStore = () => {
-    const domString = 'hi new store';
+    let domString = '<h2>Our only book:</h2>';
+    domString += '<p>Buy it now:</p>';
+    domString += `<h3>${bookInfo.price}</h3>`;
+    domString += `<img src=${bookInfo.image} alt="book cover"/>`;
+    domString += '<button id="cart-button" class="btn btn-danger col-12">Add to cart</button>'
+    
     printToDom('store-container', domString);
+    document.getElementById('cart-button').addEventListener('click', addToCartEvent);
 }
 
 export default {makeStore};
